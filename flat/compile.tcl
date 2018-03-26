15399a9af5f4c50736b4fb6648660b136f7c196c
Salil Kapur <salilkapur93@gmail.com>
2018-03-26 13:45:12 -0400

    Adding compile files. Source: https://github.com/anirudhSK/myverilog/blob/master/up_counter/compile_lite.tcl

--- flat/compile.tcl
+++ flat/compile.tcl
@@ -1,24 +1,28 @@
 # Synthesis script
 
+#Set part number
+set_part xc7a35tcpg236-1
 # Read verilog source files
 read_verilog -sv  aes.sv
-read_verilog -sv  aes_encrypt.sv
+read_verilog -sv  gcm_aes.sv
 read_verilog -sv  display.sv
+
+#Read constraints file
 read_xdc          constraints_artix_7.xdc
 
 # Run Synthesis
-synth_design -top aes -part xc7a35tcpg236-1
+synth_design -directive runtimeoptimized -top aes
 
 # Report timing
-report_timing -setup -file aes_setup_report.txt
-report_timing -hold  -file aes_hold_report.txt
-
-write_sdf -force syn_output.sdf
-write_verilog -sdf_anno true -mode timesim -force syn_output.v
+report_timing -setup -file synth_aes_setup_report.txt
+report_timing -hold  -file synth_aes_hold_report.txt
 
 place_design
 route_design
-write_sdf -force pnr_output.sdf
-write_verilog -sdf_anno true -mode timesim -force pnr_output.v
-report_timing -setup
-report_timing -hold
+
+# Report timing
+report_timing -setup -file impl_aes_setup_report.txt
+report_timing -hold  -file impl_aes_hold_report.txt
+report_timing_summary -file timing_report_aes.txt
+
+write_bitstream -force ../bitstreams/aes.bit
