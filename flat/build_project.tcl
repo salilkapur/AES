24e05108fb50ffb0e4b2919dc58b521642234000
Salil Kapur <salilkapur93@gmail.com>
2018-04-16 01:14:31 -0400

    Updating folder path for bitstream output

--- flat/build_project.tcl
+++ flat/build_project.tcl
@@ -15,15 +15,16 @@ read_xdc          constraints_artix_7.xdc
 synth_design -directive runtimeoptimized -top aes
 
 # Report timing
-report_timing -setup -file synth_aes_setup_report.txt
-report_timing -hold  -file synth_aes_hold_report.txt
+report_timing -setup -file synth_5_aes_7_gcm_setup_report.txt
+report_timing -hold  -file synth_5_aes_7_gcm_hold_report.txt
 
+opt_design
 place_design
 route_design
 
 # Report timing
-report_timing -setup -file impl_aes_setup_report.txt
-report_timing -hold  -file impl_aes_hold_report.txt
-report_timing_summary -file timing_report_aes.txt
+report_timing -setup -file impl_5_aes_7_gcm_setup_report.txt
+report_timing -hold  -file impl_5_aes_7_gcm_hold_report.txt
+report_timing_summary -file impl_5_aes_7_gcm_timing_summary.txt
 
-write_bitstream -force ../bitstreams/aes.bit
+write_bitstream -force ./bitstreams/aes.bit
