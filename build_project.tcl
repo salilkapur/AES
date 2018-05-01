c725056a5b6565af505a00fabbda1296f2588661
Salil Kapur <salilkapur93@gmail.com>
2018-05-01 18:55:27 -0400

    Updating build file

--- build_project.tcl
+++ build_project.tcl
@@ -1,33 +1,37 @@
-# Synthesis script
+# Synthesis and Implementation script
 
 #Set part number
 set_part xc7a35tcpg236-1
+
 # Read verilog source files
-read_verilog -sv  fn_aes_encrypt_stage.sv
-read_verilog -sv  gcm_aes.sv
-read_verilog -sv  aes.sv
-read_verilog -sv  display.sv
 read_verilog -sv  clk_gen.sv
+read_verilog -sv  fn_aes_encrypt_stage.sv
+read_verilog -sv  fn_aes_key_expansion.sv
+read_verilog -sv  gcm_aes.sv
+read_verilog -sv  display.sv
+read_verilog -sv  aes.sv
 
 #Read constraints file
-read_xdc          constraints_artix_7.xdc
+read_xdc constraints_artix_7.xdc
 
 # Run Synthesis
 synth_design -top aes
 
-# Report timing
-report_timing -setup -file synth_aes_setup_report.txt
-report_timing -hold  -file synth_aes_hold_report.txt
-report_timing_summary -file synth_timing_report_aes.txt -max 20
-report_utilization -file synth_utilization_report.txt
+# Reports after synthesis
+report_timing -setup  -file ./reports/synth_aes_setup_report.txt
+report_timing -hold   -file ./reports/synth_aes_hold_report.txt
+report_timing_summary -file ./reports/synth_timing_report_aes.txt
+report_utilization    -file ./reports/synth_utilization_report.txt
 
+#Run implementation
 place_design
 route_design
 
-# Report timing
-report_timing -setup -file impl_aes_setup_report.txt
-report_timing -hold  -file impl_aes_hold_report.txt
-report_timing_summary -file impl_timing_report_aes.txt -max 20
-report_utilization -file impl_utilization_report.txt
+# Reports after implementation
+report_timing -setup  -file ./reports/impl_aes_setup_report.txt
+report_timing -hold   -file ./reports/impl_aes_hold_report.txt
+report_timing_summary -file ./reports/impl_timing_report_aes.txt
+report_utilization    -file ./reports/impl_utilization_report.txt
 
+#Write bitstream
 write_bitstream -force ./bitstreams/aes.bit
